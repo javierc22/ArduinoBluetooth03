b390ef334fa0dc79d9bed9ffa346f298f638888d
javierc22 <javier.ec.22@gmail.com>
2018-12-26 00:59:16 -0300

    Agregando diseño y permisos para Bluetooth
    Se agrega permisos para usar Bluetooth y se comprueba si el dispositivo es compatible con Bluetooth

--- app/src/main/java/com/javierec/arduinobluetooth03/MainActivity.java
+++ app/src/main/java/com/javierec/arduinobluetooth03/MainActivity.java
@@ -1,13 +1,35 @@
 package com.javierec.arduinobluetooth03;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    BluetoothAdapter mBluetoothAdapter = null;
+    int ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // Comprueba si dispositivo es compatible con Bluetooth
+        if (mBluetoothAdapter == null) {
+            //Dispositivo no posee Bluetooth
+            Toast.makeText(this,
+                    "Su dispositivo no posee Bluetooth",
+                    Toast.LENGTH_SHORT).show();
+        } else if (!mBluetoothAdapter.isEnabled()) {
+            // Habilita BlueTooth
+            // Esto generará una solicitud para habilitar Bluetooth a través de los ajustes de sistema (sin detener tu aplicación)
+            // en caso que no esté activado
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, ENABLE_BT);
+        }
     }
 }
