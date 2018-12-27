4eeb60cea2708fc8571bedd8aa05d7f3149bbfc0
javierc22 <javier.ec.22@gmail.com>
2018-12-26 22:32:31 -0300

    Comprobando si conexión Bluetooth fue activada o no
    Parte 2: Se agrega lógica para comprobar si conexión Bluetooth fue activada o denegada por el usuario

--- app/src/main/java/com/javierec/arduinobluetooth03/MainActivity.java
+++ app/src/main/java/com/javierec/arduinobluetooth03/MainActivity.java
@@ -1,21 +1,34 @@
 package com.javierec.arduinobluetooth03;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // UI Elements
+    private Button buttonConectar, buttonLed1, buttonLed2, buttonLed3;
+
+    // Bluetooth
     BluetoothAdapter mBluetoothAdapter = null;
-    int ENABLE_BT = 1;
+    private static final int ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Bind UI Elements
+        buttonConectar = findViewById(R.id.button_conectar);
+        buttonLed1 = findViewById(R.id.button_led_1);
+        buttonLed2 = findViewById(R.id.button_led_2);
+        buttonLed3 = findViewById(R.id.button_led_3);
+
+        // Set Bluetooth Adapter
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         // Comprueba si dispositivo es compatible con Bluetooth
@@ -30,6 +43,28 @@ public class MainActivity extends AppCompatActivity {
             // en caso que no esté activado
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, ENABLE_BT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case ENABLE_BT:
+                if (resultCode == Activity.RESULT_OK) {
+                    Toast.makeText(this,
+                            "Bluetooth fue activado",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this,
+                            "Bluetooth NO fue activado, la aplicación se cerrará",
+                            Toast.LENGTH_SHORT).show();
+
+                    finish();
+                }
+
+                break;
         }
     }
 }
