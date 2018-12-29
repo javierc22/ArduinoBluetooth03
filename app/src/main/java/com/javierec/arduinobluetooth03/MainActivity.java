45d79fbb86e18e161d8d666d715511c78a993198
javierc22 <javier.ec.22@gmail.com>
2018-12-29 14:22:41 -0300

    Realizando conexión Bluetooth entre Aplicación y dispositivo Bluetooth
    Parte 5: Se logra conectar la aplicación con módulo Bluetooth HC-05 / HC-06

--- app/src/main/java/com/javierec/arduinobluetooth03/MainActivity.java
+++ app/src/main/java/com/javierec/arduinobluetooth03/MainActivity.java
@@ -2,12 +2,18 @@ package com.javierec.arduinobluetooth03;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,10 +22,13 @@ public class MainActivity extends AppCompatActivity {
 
     // Bluetooth
     BluetoothAdapter mBluetoothAdapter = null;
+    BluetoothDevice mDevice = null;
+    BluetoothSocket mSocket = null;
     private static final int ENABLE_BT = 1;
     private static final int ENABLE_CONECTION = 2;
-    boolean conection = false;
+    boolean myConection = false;
     private static String MAC = null;
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,8 +62,16 @@ public class MainActivity extends AppCompatActivity {
         buttonConectar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (conection) {
+                if (myConection) {
                     // Desconectar
+                    try {
+                        mSocket.close();
+                        myConection = false;
+                        buttonConectar.setText("Conectar");
+                        Toast.makeText(getApplicationContext(), "Bluetooth fue desconectado", Toast.LENGTH_SHORT).show();
+                    } catch (IOException error) {
+                        Toast.makeText(getApplicationContext(), "Error: " + error, Toast.LENGTH_SHORT).show();
+                    }
                 } else {
                     // Conectar
                     Intent intent = new Intent(MainActivity.this, ListDevices.class);
@@ -69,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         switch (requestCode) {
-            case ENABLE_BT:
+            case ENABLE_BT: // Comprueba si Bluetooth stá activado
                 if (resultCode == Activity.RESULT_OK) {
                     Toast.makeText(this,
                             "Bluetooth fue activado",
@@ -84,13 +101,29 @@ public class MainActivity extends AppCompatActivity {
 
                 break;
 
-            case ENABLE_CONECTION:
+            case ENABLE_CONECTION: //
                 if (resultCode == Activity.RESULT_OK) {
-                    MAC = data.getExtras().getString(ListDevices.ADDRESS_MAC);
 
-                    Toast.makeText(this,
-                            "MAC: " + MAC,
-                            Toast.LENGTH_SHORT).show();
+                    MAC = data.getStringExtra(ListDevices.ADDRESS_MAC);
+                    // Toast.makeText(this, "MAC: " + MAC, Toast.LENGTH_SHORT).show();
+
+                    // Obtenemos el dispositivo conectado junto con su dirección MAC y lo guardamos
+                    // para ejecutar la conexión Bluetooth
+                    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                    mDevice = mBluetoothAdapter.getRemoteDevice(MAC);
+
+                    try {
+                        mSocket = mDevice.createRfcommSocketToServiceRecord(MY_UUID);
+                        mSocket.connect();
+                        myConection = true;
+                        buttonConectar.setText("Desconectar");
+                        Toast.makeText(this, "Estás conectado con: " + MAC, Toast.LENGTH_SHORT).show();
+
+                    } catch (IOException error) {
+                        myConection = false;
+                        Toast.makeText(this, "Ocurrió un error: " + error, Toast.LENGTH_SHORT).show();
+                    }
+
                 } else {
                     Toast.makeText(this,
                             "Error al obtener dirección MAC",
